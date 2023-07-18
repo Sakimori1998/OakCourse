106bf04566b463240fb485c424df7f3c0f64830b
local_admin <local_admin@DESKTOP-CC73B34>
2023-07-18 10:59:58 +0800

    Add project files.

---
+++ DTO/UserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DTO
+{
+  public class UserDTO
+  {
+    public int ID { get; set; }
+    [Required(ErrorMessage = "Please fill the Username area")]
+    public string Username { get; set; }
+    [Required(ErrorMessage = "Please fill the Password area")]
+    public string Password { get; set; }
+    public string email { get; set; }
+    public string Imagepath { get; set; }
+    [Required(ErrorMessage = "Please fill the name area")]
+    public string Name { get; set; }
+    public bool isAdmin { get; set; }
+    [Display(Name="User Image")]
+    public HttpPostedFileBase UserImage { get; set; }
+  }
+}
